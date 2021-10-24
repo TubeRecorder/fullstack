660392b4496afdbe6c990399b70716502633caac
Bassem Girgis <bgirgis@pm.me>
2021-10-23 22:29:38 -0500

    WIP

--- db/postgres/init.sql
+++ db/postgres/init.sql
@@ -7,7 +7,3 @@ CREATE TABLE downloads (
   status int NOT NULL,
   PRIMARY KEY (link_url)
 );
-CREATE USER test_user WITH NOCREATEDB ENCRYPTED PASSWORD 'test_pass';
-GRANT ALL PRIVILEGES ON TABLE events,
-  snapshots,
-  queries TO test_user;
