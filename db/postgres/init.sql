8fb75c1bb45f6f5121a74c3a6a4cad5e11cef0bf
Bassem Girgis <brgirgis@gmail.com>
2021-10-23 23:00:00 -0500

    WIP

--- db/postgres/init.sql
+++ db/postgres/init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE tuberecorder;
-\ c tuberecorder;
+\c tuberecorder;
+
 -- a single table is used for all downloads
 CREATE TABLE downloads (
   link_url text NOT NULL,
@@ -7,3 +8,18 @@ CREATE TABLE downloads (
   status int NOT NULL,
   PRIMARY KEY (link_url)
 );
+
+CREATE
+  USER
+  db_user
+WITH
+  NOCREATEDB
+ENCRYPTED PASSWORD
+  'db_pass';
+
+GRANT
+  ALL PRIVILEGES
+ON TABLE
+  downloads
+TO
+  db_user;
